37a6766b1ac9c6e5d7270af920bbd7c39438e4b4
[hadiaalv] <hadiaalvi18@gmail.com>
2025-07-14 15:09:32 +0500

    Predict numbers

---
+++ LinearRegression.py
@@ -0,0 +1,13 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Sample data
+X = np.array([[1], [2], [3], [4], [5]])  # Hours studied
+y = np.array([1, 3, 2, 3, 5])            # Scores
+
+# Train the model
+model = LinearRegression()
+model.fit(X, y)
+
+# Predict score for 6 hours of study
+print("Predicted Score:", model.predict([[6]]))
